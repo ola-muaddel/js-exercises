f881d6f7f9666c4ea73372582e4f46f60fc259ff
ola-muaddel <o0ola.ol0100@gmail.com>
2026-06-02 23:38:27 +0300

    correct modulo operation

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 let user_input = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 for (let i = 1; i <= user_input; i++) {
 
-    if (i % 15) { // ==> i % 3 == 0 && i % 5 == 0
+    if (i % 15 === 0) { // ==> i % 3 === 0 && i % 5 === 0
         console.log("FizzBuzz");
     } else if (i % 3 === 0) {
         console.log("Fizz");
