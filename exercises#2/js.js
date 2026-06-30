108cd56af8e0e6c88708bcdd7ff2d64aa99d8b7e
ola-muaddel <o0ola.ol0100@gmail.com>
2026-06-30 19:29:48 +0300

    remove js.js to write down the code in script tag

--- exercises#2/js.js
+++
@@ -1,7 +0,0 @@
-const output = document.querySelector(".output");
-output.textContent = "";
-let i = 10;
-
-//const para = document.createElement('p');
-//para.textContent = ;
-//output.appendChild(para); 
